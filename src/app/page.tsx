8fd37b3a7f9cf49ba6c11cefa5fee187c2caa491
arowana755 <silure.38@gmail.com>
2024-10-30 09:52:11 +0100

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,16 +2,16 @@ export default function Home() {
   return (
     <body className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <form>
+        <form method="post" action="#">
           <div>
             <label htmlFor="preference">Quelles sont vos préférences ?</label>
-            <textarea id="preference"></textarea>
+            <input type="text" id="preference" value="preference>
           </div>
           <div>
             <label htmlFor="temps">Combien de temps voulez-vous y consacrer ?</label>
-            <textarea id="temps"></textarea>
+            <input type="text" id="temps" value="temps">
           </div>
-          <input type="submit">Envoyer</input>
+          <input type="submit" value="envoyer">Envoyer</input>
         </form>
       </main>
     </body>
