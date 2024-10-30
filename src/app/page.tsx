454e40a76620374b8d900e1c5ef4100792120cd7
arowana755 <silure.38@gmail.com>
2024-10-30 09:28:39 +0100

    Réecriture de la page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,10 +2,20 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+    <body className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        
+        <form>
+          <div>
+            <label htmlFor="preference">Quelles sont vos préférences ?</label>
+            <textarea id="preference"></textarea>
+          </div>
+          <div>
+            <label htmlFor="temps">Combien de temps voulez-vous y consacrer ?</label>
+            <textarea id="temps"></textarea>
+          </div>
+          <input type="submit"></input>
+        </form>
       </main>
-    </div>
+    </body>
   );
 }
