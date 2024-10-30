c0499a3decc6ae693e334f41a46137bab96a0470
arowana755 <silure.38@gmail.com>
2024-10-30 10:52:00 +0100

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,14 @@ export default function Home() {
   return (
     <body className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
+          <Image
+          className="dark:invert"
+          src="/next.svg"
+          alt="Next.js logo"
+          width={0}
+          height={0}
+          priority
+        />
         <form method="post" action="#">
           <Image
           className="dark:invert"
