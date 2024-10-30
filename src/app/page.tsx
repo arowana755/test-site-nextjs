aaaa13dcd05a784cab2e4b147a1f3ba0a95ebee8
arowana755 <silure.38@gmail.com>
2024-10-30 10:38:08 +0100

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,14 @@ export default function Home() {
     <body className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
         <form method="post" action="#">
+          <Image
+          className="dark:invert"
+          src="/next.svg"
+          alt="Next.js logo"
+          width={0}
+          height={0}
+          priority
+        />
           <div>
             <label htmlFor="preference">Quelles sont vos préférences ?</label>
             <input type="text" id="preference" value="preference>
